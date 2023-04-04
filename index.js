e55e12c137f18a3e77eeddd0ce54c1c3e38c3e9f
elizabethkerubo02 <keruboelizabeth02@gmail.com>
2023-04-04 19:18:29 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-function isPalindrome(word) {
+function reverseString(word) {
   // Write your algorithm here
-}
+  const arrayWord = word.split ("");
+  const reversearrayWord = arrayWord.reverse();
+  const reverseWord = reversearrayWord.join("");
+  return reverseWord;
+  return word;
+  }
+  
+function isPalindrome(word){
+  const reverseWord= reverseString (word);
+    if (word ===reverseString(word)){
+
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 /* 
   Add your pseudocode here
+  reverse input string
+  return true if input equals reversed string
+  return false if string isn't equal reversed string
 */
 
 /*
   Add written explanation of your solution here
-*/
+    I wrote a function that takes in an input string returns true if the reversed string matches 
+    the input string and false if otherwise.
+      Test Cases
+    Example : wow === wow // true
+              mum === mum//true
+              fool === loof//false
+  */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
